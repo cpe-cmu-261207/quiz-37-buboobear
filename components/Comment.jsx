83369394a849d61c1358d2d89b68c4f0e371dd51
bobubear <jedsa.pd@gmail.com>
2022-09-15 14:20:46 +0700

    finish *_*

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -2,12 +2,39 @@ import React from "react";
 import Reply from "./Reply";
 
 export default function Comment(props) {
-  return (
-    <div>
-      {/* Entire Comment div */}
-      <div></div>
+    return (
+        <div>
+            {/* Entire Comment div */}
+            <div className="d-flex gap-2 my-2">
+                <img
+                    src={props.data.userImagePath}
+                    width="48"
+                    height="48"
+                    className="rounded-circle"
+                    style={{ objectFit: "cover" }}
+                />
+                <div
+                    className="rounded rounded-3 p-2"
+                    style={{ backgroundColor: "#E5E7EB" }}
+                >
+                    <span className="fw-semibold">{props.data.username}</span>
+                    <br />
+                    <span>{props.commentText}</span>
+                    {props.data.likeNum > 0 && (
+                        <div className="d-flex align-items-center gap-1">
+                            <img src="/like.svg" width={20}></img>
+                            <span className="text-muted">
+                                {props.data.likeNum}
+                            </span>
+                        </div>
+                    )}
+                </div>
+            </div>
 
-      {/*render Reply here... */}
-    </div>
-  );
+            {/*render Reply here... */}
+            {props.data.replies.map((element, index) => (
+                <Reply data={element} key={index} />
+            ))}
+        </div>
+    );
 }
--- components/PostOwner.jsx
+++ components/PostOwner.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import Comment from "./Comment";
 
 export default function PostOwner(props) {
-  return <div></div>;
+    return (
+        <div className="vstack gap-3">
+            <div className="d-flex align-items-center gap-3">
+                <img
+                    src="/profileImages/110146857_3353871798170740_6103571106592671618_n.jpg"
+                    width="48"
+                    height="48"
+                    className="rounded-circle"
+                    style={{ objectFit: "cover" }}
+                />
+                <span className="fw-semibold fs-5">
+                    Jedsadaporn Juntong 640612179
+                </span>
+            </div>
+            <span>Jongin is God!!</span>
+            <div className="d-flex align-items-center gap-1">
+                <img src="/like.svg" width={20}></img>
+                <span className="text-muted">5k คน</span>
+            </div>
+            <hr className="m-0" />
+            {props.comment.map((data, index) => (
+                <Comment data={data} key={index} />
+            ))}
+        </div>
+    );
 }
